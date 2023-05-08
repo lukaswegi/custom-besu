f74da2bd11701e2a5563b8e1dcf8d77894852cb8
Lukas Wegscheider <lukaswegi@gmail.com>
2023-05-08 21:36:01 +0200

    removed withdrawal check

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/AbstractEngineForkchoiceUpdated.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/internal/methods/engine/AbstractEngineForkchoiceUpdated.java
@@ -240,13 +240,12 @@ public abstract class AbstractEngineForkchoiceUpdated extends ExecutionEngineJso
       final Optional<List<Withdrawal>> maybeWithdrawals,
       final BlockHeader headBlockHeader) {
 
-    final boolean newTimestampGreaterThanHead = payloadAttributes.getTimestamp() > headBlockHeader.getTimestamp();
-    if (headBlockHeader != null) {
-      return newTimestampGreaterThanHead;
-    }
-    return newTimestampGreaterThanHead && getWithdrawalsValidator(
-              protocolSchedule, headBlockHeader, payloadAttributes.getTimestamp())
-            .validateWithdrawals(maybeWithdrawals);
+    final boolean newTimestampGreaterThanHead =
+        payloadAttributes.getTimestamp() > headBlockHeader.getTimestamp();
+    return newTimestampGreaterThanHead;
+        /*&& getWithdrawalsValidator(
+                protocolSchedule, headBlockHeader, payloadAttributes.getTimestamp())
+            .validateWithdrawals(maybeWithdrawals)*/
   }
 
   private JsonRpcResponse handleNonValidForkchoiceUpdate(
